b0c4b0d287e95f7b242f047fb5b75cadc7a865e9
Bala Raman <srbala@gmail.com>
2021-12-26 23:52:41 -0500

    code reorg

--- utils/java/liberica.js
+++ utils/java/liberica.js
@@ -1,6 +1,10 @@
 // https://api.bell-sw.com/v1/liberica/releases?version-modifier=latest&bitness=64&release-type=lts&os=linux&package-type=tar.gz&bundle-type=jre&version-feature=8
 // https://api.bell-sw.com/v1/liberica/releases?version-modifier=latest&bitness=64&release-type=lts&os=linux&package-type=tar.gz&bundle-type=jdk&version-feature=11
-//console.log(process.argv);
+//
+// https://api.github.com/repos/corretto/corretto-11/releases
+// https://api.github.com/repos/corretto/corretto-11/releases/latest
+//
+// console.log(process.argv);
 if (process.argv.length == 5) {
     const axios = require('axios');
     const jdk_bundle = process.argv[2];
@@ -8,13 +12,11 @@ if (process.argv.length == 5) {
     const jdk_arch = process.argv[4];
     axios.get('https://api.bell-sw.com/v1/liberica/releases?version-modifier=latest&bitness=64&release-type=lts&os=linux&package-type=tar.gz&version-feature=' + jdk_version +'&bundle-type=' + jdk_bundle +'&arch=' + jdk_arch).then(resp => {
 
-        //const res = JSON.parse(resp.data.length);
-        // console.log(resp.data);
-        // console.log(process.argv);
-        console.log(resp.data[0]);
-        console.log(resp.data[0].version);
-        console.log(resp.data[0].sha1);
-        console.log(resp.data[0].downloadUrl);
+    console.log(resp.data[0]);
+    console.log("");   
+    console.log("version=" + resp.data[0].version.replace(/\+/ig, '.')); 
+    console.log("filesha=" + resp.data[0].sha1);
+    console.log("fileurl=" + resp.data[0].downloadUrl);
     });
 } else {
     console.log("Invalid input args")
